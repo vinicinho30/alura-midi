c419419bc7faa55901c964647f46f39f0d9a75c7
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2023-09-12 13:11:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-<audio src="sounds/keyq.wav" id="som_tecla_pom"></audio>
+function tocaSomPom () {
 document.querySelector('#som_tecla_pom'.play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
